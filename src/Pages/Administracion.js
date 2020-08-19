3be92ef40dc25ca1dc75640fa0c07c45a523129b
unknown <alansm9790@gmail.com>
2020-08-19 14:29:28 -0500

    Pruebas

---
+++ src/Pages/Administracion.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Administracion = () => {
+    return <div>
+        <form>
+            <input type="password" />   
+            <button>Ir</button>
+        </form>
+    </div>
+}
